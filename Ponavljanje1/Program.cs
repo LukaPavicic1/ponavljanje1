1874800eea855a45103e01e4c9b512a8ef9a8cd2
LukaPavicic1 <91119924+LukaPavicic1@users.noreply.github.com>
2022-09-12 12:42:22 +0200

    Add files via upload

---
+++ Ponavljanje1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ponavljanje1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 100;
+            int b = 3;
+            Console.WriteLine(a + " dijeljeno s " + b + " je " + a/b + " i ostatak " + a%b);
+            Console.ReadKey();
+        }
+    }
+}
